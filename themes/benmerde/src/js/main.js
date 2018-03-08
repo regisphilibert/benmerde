93118643ec9ff368064ccfd0bbc0e86d361ec9aa
Regis Philibert <login@regisphilibert.com>
2018-03-08 10:46:26 -0500

    clear search on close search

--- themes/benmerde/src/js/main.js
+++ themes/benmerde/src/js/main.js
@@ -1,7 +1,15 @@
+var searchIsOpen = false;
 $('.header-search').click(function(){
   $('.bm-Search').addClass('animating').slideToggle(200, function(){
     $(this).removeClass('animating')
     $('.header-search').toggleClass('is-active')
+    if(searchIsOpen){
+      search.helper.setQuery('').search();
+      $('.bm-Search__box__input').val('')
+      searchIsOpen = false;
+    } else {
+      searchIsOpen = true
+    }
   })
 })
 
