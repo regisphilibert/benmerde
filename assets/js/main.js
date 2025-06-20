10921d9082b602128a1807bacf4913f854b245be
Regis Philibert <login@regisphilibert.com>
2025-06-20 14:37:26 -0400

    Implement Pagefind Fixes #9

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,30 @@
 
 
-$('.header-search').click(function(){
-  $('.bm-Search').addClass('animating').slideToggle(200, function(){
-    $(this).removeClass('animating')
-    $('.header-search').toggleClass('is-active')
-    if(searchIsOpen){
-      search.helper.setQuery('').search();
-      $('.bm-Search__box__input').val('')
-      searchIsOpen = false;
-    } else {
-      searchIsOpen = true
-    }
-  })
-})
+document.addEventListener('DOMContentLoaded', (event) => {
+    let bmSearch = new PagefindUI({
+      element: "#search-box",
+      autofocus: true,
+      showImages: true,
+      showEmptyFilters: false,
+      translations: {
+        placeholder: "Vous cherchez quelque chose?",
+      },
+      openFilters: window.innerWidth > 768 ? ['Pays', 'Quoi'] : undefined,
+      processResult: function (result) {
+        //console.log(result)
+      }
+    });
+    var searchIsOpen = false;
+    $('.header-search').click(function(){
+      $('.bm-Search').addClass('animating').slideToggle(200, function(){
+        $(this).removeClass('animating')
+        $('.header-search').toggleClass('is-active')
+        if(searchIsOpen){
+          searchIsOpen = false;
+        } else {
+          searchIsOpen = true
+        }
+        console.log({searchIsOpen})
+      })
+    })
+});
--- content/post/usa/_index.md
+++ content/post/usa/_index.md
@@ -3,6 +3,9 @@ title: USA
 slogan: je suis aux USA
 slogan_past: j'étais aux USA
 url: usa/
+cascade:
+  residency:
+  - USA
 menu:
   projets:
     identifier: usa
